f4507efe4f50ca0cdf552d1f10eee5bfd60e9565
dnivanthaka <nivanthaka@gmail.com>
2016-07-14 12:51:40 +0530

    Improved the code and added a .gitignore

--- mpu6050.c
+++ mpu6050.c
@@ -1,7 +1,9 @@
 /*
  * Sample MPU6050 I2C Driver
  *
- *
+ *  References 
+ *  http://lxr.free-electrons.com/source/include/uapi/asm-generic/errno-base.h
+ *  http://lxr.free-electrons.com/source/Documentation/i2c/writing-clients
  */
 
 #include <linux/module.h>
@@ -11,13 +13,23 @@
 #define MPU6050_NAME "mpu6050"
 #define MPU6050_I2C_ADDRESS 0x68
 
+#define MPU6050_REG_WHOAMI 0x75
+
 static const unsigned short normal_i2c[] = {
     MPU6050_I2C_ADDRESS,
     I2C_CLIENT_END
 };
+//TODO Improve this structure
+struct mpu6050_device {
+    struct i2c_client *client;
+    struct mutex lock;
+};
 
 static int mpu6050_i2c_detect(struct i2c_client *client, 
         struct i2c_board_info *info){
+
+    //struct i2c_adapter *adapter = client->adapter;
+
     if(client->addr != MPU6050_I2C_ADDRESS)
         return -ENODEV;
 
@@ -29,14 +41,42 @@ static int mpu6050_i2c_detect(struct i2c_client *client,
 static int mpu6050_i2c_probe(struct i2c_client *client, 
         const struct i2c_device_id *id){
 
+    struct i2c_adapter *adapter = client->adapter;
+    int ret;
+    int error;
+
     dev_info(&client->dev, "Probe MPU6050\n");
+
+    if(!i2c_check_functionality(adapter, I2C_FUNC_SMBUS_BYTE_DATA) && 
+            !i2c_check_functionality(adapter, I2C_FUNC_SMBUS_I2C_BLOCK))
+        return -EIO;
+
+    //TODO Reset device here
+
+    ret = i2c_smbus_read_byte_data(client, MPU6050_REG_WHOAMI);
+    if(ret < 0){
+        dev_err(&client->dev, "Failed to detect device\n");
+        error = -ENXIO;
+        goto err_free_res;
+    }
+
+    if(ret != MPU6050_I2C_ADDRESS){
+        dev_err(&client->dev, "Failed to identify MPU6050\n");
+        error = -ENXIO;
+        goto err_free_res;
+    }
+
+err_free_res:
+
     return 0;
 }
+
 
 static int mpu6050_i2c_remove(struct i2c_client *client){
     dev_info(&client->dev, "Remove MPU6050\n");
     return 0;
 }
+
 
 static const struct i2c_device_id mpu6050_id[] = {
     {MPU6050_NAME, 0},
